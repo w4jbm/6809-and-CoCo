6356bbddf8728779bb1a858d3224b0c32ea43ea0
James McClanahan <jim30109@gmail.com>
2018-04-28 15:24:50 -0400

    Update scrollx.asm

--- FooRaceway RC2018/scrollx.asm
+++ FooRaceway RC2018/scrollx.asm
@@ -1,5 +1,5 @@
 ;
-; Downward Scroll Routine
+; Downward Scroll and Draw Car Routines
 ;
 ; by James McClanahan W4JBM
 ;
@@ -11,68 +11,68 @@
 ; of the X and Y registers.
 ;
 sfrom	equ $5de+2		; where do we start copying from?
-sto		equ $5fe+2		; where do we start copying to?
+sto	equ $5fe+2		; where do we start copying to?
 done	equ $400		; where do we stop copying?
 ;
 ; 
-		org	16000
+	org	16000
 ;
 ;***** Start of Program
 ;
 ;***** Load Constants into X and Y
 ;
 start:	LDX #sfrom
-		LDY #sto
+	LDY #sto
 ;
 ;*** Main Loop (copies from address in X to address in Y)
 loop:	LDA ,-Y
-		BMI skip
-		LDA	,-X
+	BMI skip
+	LDA	,-X
 store:	STA	,Y
-		BRA test
+	BRA test
 skip	LEAX -1,X
 test:	CMPX #done
-		BHI	loop
-		RTS
+	BHI	loop
+	RTS
 		
-		org 16030
+	org	16030
 ;		
 ;*** Draw Car
 draw:	LDX #1455
 row1:	LDA #96
-		STA ,X+
-		LDA #133
-		STA ,X+
-		LDA #136
-		STA ,X+
-		LDA #132
-		STA ,X+
-		LDA #138
-		STA ,X+
-		LDA #96
-		STA ,X+
-		LEAX 32-6,X
+	STA ,X+
+	LDA #133
+	STA ,X+
+	LDA #136
+	STA ,X+
+	LDA #132
+	STA ,X+
+	LDA #138
+	STA ,X+
+	LDA #96
+	STA ,X+
+	LEAX 32-6,X
 row2:	STA ,X+
-		LDA #143
-		STA ,X+
-		LDA #129
-		STA ,X+
-		LDA #130
-		STA ,X+
-		LDA #143
-		STA ,X+
-		LDA #96
-		STA ,X+
-		LEAX 32-6,X
+	LDA #143
+	STA ,X+
+	LDA #129
+	STA ,X+
+	LDA #130
+	STA ,X+
+	LDA #143
+	STA ,X+
+	LDA #96
+	STA ,X+
+	LEAX 32-6,X
 row3:	STA ,X+
-		LDA #133
-		STA ,X+
-		LDA #128
-		STA ,X+
-		STA ,X+
-		LDA #138
-		STA ,X+
-		LDA #96
-		STA ,X+
-		RTS 
+	LDA #133
+	STA ,X+
+	LDA #128
+	STA ,X+
+	STA ,X+
+	LDA #138
+	STA ,X+
+	LDA #96
+	STA ,X+
+	RTS 
 end:
